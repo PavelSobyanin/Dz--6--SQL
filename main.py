efa50bdbe7ffa2b828ca4424047f67a82fd0900e
Pavel Sobianin <pavel.sobyanin.git@gmail.ru>
2023-02-28 11:48:41 +0300

    Addition

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 import json
 
-from models import create_tables, Publisher, Sale, Book, Stock, Shop
+from models import create_tables, Publisher, Book, Stock, Shop
 
 SQLsystem = 'postgresql'
 login = 'postgres'
@@ -21,26 +21,28 @@ create_tables(engine)
 with open('tests_data.json', 'r') as db:
     data = json.load(db)
 
-for line in data:
-    method = {
-        'publisher': Publisher,
-        'shop': Shop,
-        'book': Book,
-        'stock': Stock,
-        'sale': Sale,
-    }[line['model']]
-    session.add(method(id=line['pk'], **line.get('fields')))
 
-session.commit()
+def searching_publisher_name():
+    query_join = session.query(Shop).join(Stock).join(Book).join(Publisher)
+    query_publisher_name = input('Введите имя (name) издателя: ')
+    query_result = query_join.filter(Publisher.publisher_name == query_publisher_name)
+    for result in query_result.all():
+        print(f'Издатель "{query_publisher_name}" найден в магазине "{result.name}" с идентификатором {result.id}')
 
-publ_name = input('Ведите имя писателя или id для вывода: ')
-if publ_name.isnumeric():
-    for c in session.query(Publisher).filter(
-            Publisher.id == int(publ_name)).all():
-        print(c)
-else:
-    for c in session.query(Publisher).filter(
-            Publisher.name.like(f'%{publ_name}%')).all():
-        print(c)
+
+def searching_publisher_id():
+    query_join = session.query(Shop).join(Stock).join(Book).join(Publisher)
+    query_publisher_name = input('Введите идентификатор (id) издателя: ')
+    query_result = query_join.filter(Publisher.id_publisher == query_publisher_name)
+    for result in query_result.all():
+        print(
+            f'Издатель c id: {query_publisher_name} найден в магазине "{result.name}" '
+            f'с идентификатором {result.id}')
+
+
+if __name__ == '__main__':
+    searching_publisher_name()
+    searching_publisher_id()
+
 
 session.close()
